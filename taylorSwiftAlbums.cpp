6bbe48069233db9a81729fd509910ba25d538b98
mi55a <milymendez678@gmail.com>
2024-06-07 23:52:37 -0500

    message

---
+++ taylorSwiftAlbums.cpp
@@ -0,0 +1,47 @@
+
+// This program asks for a Taylor Swift album from the user and returns information about the album
+// needed libraries
+#include <iostream>
+#include <string>
+#include <cctype>
+
+
+
+using namespace std;
+
+string LowerString(const string& str) {
+    string lowerStr = str;
+    for(char &c : lowerStr) {
+        c = tolower(static_cast<unsigned char>(c));
+    }
+    return lowerStr;
+}
+
+int main() {
+
+    string albumName;
+
+    int numAlbum[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    string titles[] = {"Taylor Swift", "Fearless", "Speak Now", "Red", "1989", "reputation", "Lover", "folklore", "evermore", "Midnights", "The Tortured Poets Department"};
+
+    string taylorsVersion[] = {"False", "True", "True", "True", "True", "False", "True", "True", "True", "True", "True"};
+
+
+    cout << "Welcome user to the Taylor Swift Album Info program. The program will ask you for a Taylor Swift album and return information about the album!" << endl;
+    cout << "Disclaimer: To respect Swift's work, I will be using Taylors Version for the stolen albums." << endl;
+
+    getline(cin, albumName);
+
+    string lowAlbum = LowerString(albumName);
+
+    for(int i = 0; i < 11; i++) {
+        if(lowAlbum == LowerString(titles[i])) {
+            cout << "Album number: " << numAlbum[i] << endl;
+            cout << "Album title: " << titles[i] << endl;
+            cout << "Is it Taylor's Version (owned by Taylor)? " << taylorsVersion[i] << endl;
+        }
+    }
+
+}
+
